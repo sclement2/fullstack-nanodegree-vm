5aee0e48e829ba057c4e25fd8f811495d8c98e74
Scott Clement <sclement2@gmail.com>
2016-07-01 19:33:19 -0700

    Corrected errors in database_setup.py file

--- fullstack/database_setup.py
+++ fullstack/database_setup.py
@@ -12,10 +12,10 @@ Base = declarative_base()
 class Restaurant(Base):
 	__tablename__='restaurant'
 
-	name = Column(String(80), nullable=False)
+	name = Column(String(250), nullable=False)
 	id = Column(Integer, primary_key=True)
 
-class MenuItems(Base):
+class MenuItem(Base):
 	__tablename__='menu_item'
 
 	name = Column(String(80), nullable=False)
@@ -23,8 +23,7 @@ class MenuItems(Base):
 	course = Column(String(250))
 	description = Column(String(250))
 	price = Column(String(8))
-	restaurant_id = Column( Integer, ForeignKey( 'restaurant_id' ))
-
+	restaurant_id = Column( Integer, ForeignKey( 'restaurant.id' ))
 	restaurant = relationship(Restaurant)
 
 
